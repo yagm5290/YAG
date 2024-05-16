ea79ed1a3250bc3b7533a7a417fd56b0eb101410
yagm5290 <ygomezm@dian.gov.co>
2024-05-16 08:53:59 -0500

    Update CartItem.cs

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -1,5 +1,4 @@
-﻿
-//Cambio 20240516 local
+﻿//Cambio 20240516 local_1
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
