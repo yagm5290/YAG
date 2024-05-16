3f5848fa0ca8ea3f72a7a2369def2c6ceaff9c8f
yagm5290 <yagm5290@hotmail.com>
2024-05-16 08:52:11 -0500

    Items lc

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -1,4 +1,6 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+﻿
+//Cambio 20240516 local
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
